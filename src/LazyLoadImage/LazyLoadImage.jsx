62b8e1f8bb81f9575002e5b589d755ed6db6be61
helaoutar <elaoutarhamza@gmail.com>
2020-06-21 21:29:01 +0100

    jest setup

---
+++ src/LazyLoadImage/LazyLoadImage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const LazyLoadImage = ({ src }) => {
+  return <img src={src} />
+}
+
+export default LazyLoadImage
---
+++ src/LazyLoadImage/LazyLoadImage.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from "enzyme";
+import LazyLoadImage from './LazyLoadImage';
+
+describe('LazyLoadImage', () => {
+  it('should render an img', () => {
+    const wrapper = shallow(<LazyLoadImage src="src" />);
+    expect(wrapper.find('img').exists()).toBe(true);
+  });
+});

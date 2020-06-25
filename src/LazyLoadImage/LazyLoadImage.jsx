bb7de9e48fa10d65dec3792ec8fb2cdda2680c07
helaoutar <elaoutarhamza@gmail.com>
2020-06-25 11:54:54 +0100

    Added a couple of UT

--- src/LazyLoadImage/LazyLoadImage.jsx
+++ src/LazyLoadImage/LazyLoadImage.jsx
@@ -1,6 +1,36 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable react/require-default-props */
+import React, { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import propTypes from 'prop-types';
 
-const LazyLoadImage = ({ src }) => <img src={src} alt="" />;
+const LazyLoadImage = ({
+  src,
+  container,
+  loader,
+  onLoad,
+  ...rest
+}) => {
+  const [ref, inView] = useInView({ root: container || window, threshold: 0 });
+  const [img, setImg] = useState('');
+
+  useEffect(() => { setImg(''); }, [src]);
+
+  useEffect(() => {
+    if (src && !img && inView) {
+      setImg(src);
+    }
+  }, [inView, img, src]);
+
+  return !img ? <div ref={ref}>{loader}</div> : <img src={img} alt="" onLoad={onLoad} {...rest} />;
+};
+
+LazyLoadImage.propTypes = {
+  src: propTypes.string.isRequired,
+  container: propTypes.object,
+  loader: propTypes.object,
+  onLoad: propTypes.func,
+};
 
 export default LazyLoadImage;
--- src/LazyLoadImage/LazyLoadImage.test.jsx
+++ src/LazyLoadImage/LazyLoadImage.test.jsx
@@ -5,8 +5,14 @@ import { shallow } from 'enzyme';
 import LazyLoadImage from './LazyLoadImage';
 
 describe('LazyLoadImage', () => {
-  it('should render an img', () => {
+  it('should not render an img when the element isn"t in the view', () => {
     const wrapper = shallow(<LazyLoadImage src="src" />);
-    expect(wrapper.find('img').exists()).toBe(true);
+    expect(wrapper.find('img').exists()).toBe(false);
+  });
+
+  it('should render a loader when the element isn"t in the view', () => {
+    const loader = <div className="loader" />;
+    const wrapper = shallow(<LazyLoadImage src="src" loader={loader} />);
+    expect(wrapper.find('.loader').exists()).toBe(true);
   });
 });
